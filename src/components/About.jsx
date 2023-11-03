393dcf7431493dfb3afff60eb0101cdea9c27329
Zach Nash <afreakenturtle@gmail.com>
2023-11-03 13:07:16 -0400

    corrected some technologies and about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <motion.div
-      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
       <div
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
           {title}
@@ -35,11 +37,20 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-Hello, I'm Zach, a passionate Front-End Web Developer and UI/UX Designer with a strong foundation in creating exceptional user experiences. With a keen eye for design and a love for code, I specialize in crafting visually stunning and highly functional websites and applications. <br />
-My journey in the world of web development began three years ago.
-In my toolbox, you'll find HTML5, CSS3, React, Tailwind CSS, and Bootstrap, which I use to bring design concepts to life and create seamless user interfaces. But I'm not just limited to the front end – I also possess strong back-end development skills, particularly in Express, Node.js, SQL, and Sequelize, allowing me to build complete, end-to-end solutions.      </motion.p>
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        Hello, I'm Zach, a passionate Front-End Web Developer and UI/UX Designer
+        with a strong foundation in creating exceptional user experiences. With
+        a keen eye for design and a love for code, I specialize in crafting
+        visually stunning and highly functional websites and applications.
+        My journey in the world of web development began three years ago. In my
+        toolbox, you'll find HTML5, CSS3, React, Tailwind CSS, and Bootstrap,
+        which I use to bring design concepts to life and create seamless user
+        interfaces. But I'm not just limited to the front end – I also possess
+        strong back-end development skills, particularly in Express, Node.js,
+        SQL, and Sequelize, allowing me to build complete, end-to-end solutions.{" "}
+      </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
@@ -50,4 +61,4 @@ In my toolbox, you'll find HTML5, CSS3, React, Tailwind CSS, and Bootstrap, whic
   );
 };
 
-export default SectionWrapper(About, 'about');
+export default SectionWrapper(About, "about");
