e9fd8f45bbe034ef0d5d0d0f288df4a552f68622
Zach Nash <afreakenturtle@gmail.com>
2024-03-04 13:47:46 -0500

    updated the contact form

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,8 +3,8 @@ import nairobi from './backgrounds/nairobi.png';
 import whiteabstract from './backgrounds/white-abstract.png';
 import worldmap from './backgrounds/world-map.png';
 
-import logo from './logo/logo-black.png';
-import logotext from './logo/logo-text-black.png';
+import logo from './logo/logo.png';
+import logotext from './logo/logo-text-removebg.png';
 
 import zach from './personal/zach.jpg';
 
@@ -38,17 +38,11 @@ import graphql from './tech/graphql.png';
 import postgresql from './tech/postgresql.png';
 import rubyrails from './tech/rubyrails.png';
 
-import coverhunt from './company/coverhunt.png';
-import dcc from './company/dcc.png';
-import kelhel from './company/kelhel.png';
-import microverse from './company/microverse.png';
-
 import komikult from './projects/komikult.png';
 import leaderboard from './projects/leaderboard.png';
 import math from './projects/math-magicians.png';
 import movie from './projects/movie-metro.png';
 import nyeusi from './projects/nyeusi.png';
-import space from './projects/space-hub.png';
 
 export {
   bwmap,
@@ -91,9 +85,4 @@ export {
   math,
   movie,
   nyeusi,
-  space,
-  coverhunt,
-  dcc,
-  kelhel,
-  microverse,
 };
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'Portfolio_contact', // paste your ServiceID here (you'll get one when your service is created).
+        'gmail_temp', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Zach Nash', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'afreakenturtle@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        'pDsWnxVEEuEVFBatl' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
@@ -104,6 +104,7 @@ const Contact = () => {
           <label className="flex flex-col">
             <span className="text-timberWolf font-medium mb-4">
               Your Message
+              *Please re-enter the email into message section
             </span>
             <textarea
               rows="7"
